a5d9a499981e06f9374b7da89d2af05c63fb8682
yannan.lin <zzlyn321@hotmail.com>
2017-10-04 12:13:37 -0400

    Set up for refactor

---
+++ src/main/java/web/httpWrapper.java
@@ -0,0 +1,70 @@
+package web;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import local.Analyzer;
+
+public class httpWrapper {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("test run");
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+		server.createContext("/analyze", new MyHandler());
+		server.start();
+	}
+
+	static class MyHandler implements HttpHandler {
+		public void handle(HttpExchange t) throws IOException {
+			String response = "";
+			String query = t.getRequestURI().getQuery();
+
+			HashMap<String, String> components = (HashMap<String, String>) queryToMap(query);
+
+			String postUrl = components.get("postUrl");
+
+			System.out.println(postUrl);
+
+			try {
+				response = Analyzer.urlListen(postUrl);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+
+			if (t.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+				t.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
+				t.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type,Authorization");
+				t.sendResponseHeaders(204, -1);
+				return;
+			}
+
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+	}
+
+	public static Map<String, String> queryToMap(String query) {
+		Map<String, String> result = new HashMap<String, String>();
+		for (String param : query.split("&")) {
+			String pair[] = param.split("=");
+			if (pair.length > 1) {
+				result.put(pair[0], pair[1]);
+			} else {
+				result.put(pair[0], "");
+			}
+		}
+		return result;
+	}
+
+}
